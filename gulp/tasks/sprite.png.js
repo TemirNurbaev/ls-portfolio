e4005b91754d2b5dc18e97bdb4d8f2b9118b4890
TemirNurbaev <temir1.618@gmail.com>
2016-10-02 01:44:47 +0300

    page welcome

--- gulp/tasks/sprite.png.js
+++ gulp/tasks/sprite.png.js
@@ -4,8 +4,9 @@ module.exports = function() {
 	$.gulp.task('sprite:png', function () {
 	  var spriteData = $.gulp.src('./source/sprite/*.png').pipe($.gp.spritesmith({
 	    imgName: 'sprite.png',
-	    cssName: 'sprite.css',
-	    imgPath: '/assets/img/sprite.png'
+	    cssName: 'sprite.scss',
+	    cssFormat: 'css',
+	    imgPath: '../img/'
 	  }));
 	  return spriteData.img.pipe($.gulp.dest($.config.root + '/assets/img')),
 	  		 spriteData.css.pipe($.gulp.dest('./source/style/common'));
