26f9a75917be4f0cf91523f72aab74b095e2179f
TemirNurbaev <temir1.618@gmail.com>
2016-10-03 11:20:48 +0300

    Осталось доверстать  Мои работы

--- source/js/app.js
+++ source/js/app.js
@@ -1,7 +1,15 @@
-(function() {
-  'use strict';
+// (function() {
+//   'use strict';
 
-  setTimeout(function() {
-    document.querySelector('.greating_picture').classList.add('m--show');
-  }, 1000);
-})();
+//   setTimeout(function() {
+//     document.querySelector('.greating_picture').classList.add('m--show');
+//   }, 1000);
+// })();
+
+
+$(document).ready(function(){
+	$(".auth-link").on('click', function(){
+    	S(".flipper").css("transform", "rotateY(180deg)");
+	});
+
+});
